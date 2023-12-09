fb0ece6350db76033b1c010e15d22b791e7e309c
ageu_dev19 <ageumenezes423@gmail.com>
2023-12-09 15:43:48 -0300

    Add NextJs as my new HardSkill

--- src/components/MyHardSkills/index.tsx
+++ src/components/MyHardSkills/index.tsx
@@ -8,34 +8,35 @@ export function MyHardSkills() {
           <h2>My Hard Skills</h2>
         </div>
         <h4>Web Developer</h4>
-          <ul>
-            <li><p className='devicon-html5-plain-wordmark colored'>HTML5 | Intermediate</p></li>
-            <li><p className='devicon-css3-plain-wordmark colored'>CSS3 | Intermediate</p></li>
-            <li><p className='devicon-javascript-plain colored'> JavaScript | Intermediate</p></li>
-            <li><p className='devicon-react-original-wordmark colored'>Intermediate</p></li>
-            <li><p className='devicon-git-plain-wordmark colored'>Intermediate</p></li>
-            <li><p className='devicon-github-original-wordmark colored'> Intermediate</p></li>
-            <li><p className='devicon-vscode-plain colored'> VS Code | Intermediate</p></li>
-            <li><p className='devicon-bash-plain colored'> Bash | Intermediate</p></li>
-            <li><p className='devicon-jest-plain colored'> Jest | Basic</p></li>
-          </ul>
-            </div>
-        <div>
-          <h4>Creativity</h4>
-          <ul>
-            <li><p className='devicon-premierepro-plain colored'> Intermediate</p></li>
-            <li><p className='devicon-aftereffects-plain colored'> Basic</p></li>
-            <li><p className='devicon-photoshop-plain colored'> Intermediate</p></li>
-            <li><p className='devicon-illustrator-plain colored'> Intermediate</p></li>
-          </ul>
-        </div>
-        <div>
-          <h4>Others</h4>
-          <ul>
-            <li><i><p className='devicon-apple-original colored'>MacOS</p></i></li>
-            <li><p style={{color: 'black'}}>🇺🇸 English: Advanced</p></li>
-          </ul>
-        </div>
+        <ul>
+          <li><p className='devicon-html5-plain-wordmark colored'>HTML5 | Intermediate</p></li>
+          <li><p className='devicon-css3-plain-wordmark colored'>CSS3 | Intermediate</p></li>
+          <li><p className='devicon-javascript-plain colored'> JavaScript | Intermediate</p></li>
+          <li><p className='devicon-react-original-wordmark colored'>Intermediate</p></li>
+          <li><p className='devicon-nextjs-plain-wordmark colored'>Basic</p></li>
+          <li><p className='devicon-git-plain-wordmark colored'>Intermediate</p></li>
+          <li><p className='devicon-github-original-wordmark colored'> Intermediate</p></li>
+          <li><p className='devicon-vscode-plain colored'> VS Code | Intermediate</p></li>
+          <li><p className='devicon-bash-plain colored'> Bash | Intermediate</p></li>
+          <li><p className='devicon-jest-plain colored'> Jest | Basic</p></li>
+        </ul>
+      </div>
+      <div>
+        <h4>Creativity</h4>
+        <ul>
+          <li><p className='devicon-premierepro-plain colored'> Intermediate</p></li>
+          <li><p className='devicon-aftereffects-plain colored'> Basic</p></li>
+          <li><p className='devicon-photoshop-plain colored'> Intermediate</p></li>
+          <li><p className='devicon-illustrator-plain colored'> Intermediate</p></li>
+        </ul>
+      </div>
+      <div>
+        <h4>Others</h4>
+        <ul>
+          <li><i><p className='devicon-apple-original colored'>MacOS</p></i></li>
+          <li><p style={{color: 'black'}}>🇺🇸 English: Advanced</p></li>
+        </ul>
+      </div>
     </HardSkillsStyle>
   )
 }
