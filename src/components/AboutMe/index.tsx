16eaae30aa5d66c70257f3ba59ff5bf35ec5e32b
ageu_dev19 <ageumenezes423@gmail.com>
2023-12-20 10:21:20 -0300

    Update the AboutMe component info

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,17 +1,61 @@
 import { AboutMeSection } from "./styles";
 
 export function AboutMe() {
-  return(
+  return (
     <AboutMeSection>
       <h2>About me</h2>
-      <p>Front-end Web Developer, I started my studies at <a href="https://www.betrybe.com/" target="_blank" style={{color: 'var(--box-shadow)', textDecoration: 'none'}}><b>Trybe</b></a>, a Computer Science school, for 6 to 7 months.
-          With the knowledge acquired there, I was able to start my career as a Software Engineer. I develop this portfolio as well as other projects, and I am continuing my studies at <a href="https://www.rocketseat.com.br" target="_blank" style={{color: 'var(--background-3)', textDecoration: 'none'}}><b>Rocketseat</b></a>, <a href="https://go.hotmart.com/O61634593X" target="_blank" style={{color: 'var(--background-5)', textDecoration: 'none'}}><b>Cataline</b></a> and others.</p><br/>
-
-      <p>Brazilian, I'm 21 years old and I live in the state of Ceará. I like to watch science fiction,
-        animation and adventure movies. I Also like to appreciate nature, enjoy international and electronic music.</p><br/>
-
-      <p>I chose to be a Software Engineer because it is one of the great pillars of the tech world that im so fond of, and I identified with it.
-        Hence, I dedicate myself developing my hard skills, soft skills and projects to keep up with the job market.</p>
+      <p>
+        Greetings! I'm a Front-end Web Developer who embarked on my coding journey at{' '}
+        <a
+          href="https://www.betrybe.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="trybeLink"
+        >
+          <b>Trybe</b>
+        </a>, a distinguished Computer Science school, dedicating 7 months to refining my skills.
+        With the knowledge gained, I launched my career as a Software Engineer, crafting not only
+        this portfolio but also various other projects. My learning journey extends to platforms
+        such as{' '}
+        <a
+          href="https://www.rocketseat.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rocketLink"
+        >
+          <b>Rocketseat</b>
+        </a>,{' '}
+        <a
+          href="https://go.hotmart.com/O61634593X"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="catalineLink"
+        >
+          <b>Cataline</b>
+        </a> and{' '}
+        <a
+          href="https://www.udemy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="udemyLink"
+        >
+          <b>Udemy</b>
+        </a>.
+      </p>
+      <br />
+      <p>
+        Hailing from the state of Ceará, Brazil, I'm a 21-year-old tech enthusiast. When I'm not
+        immersed in code, you'll find me captivated by physics, science fiction, animation, and
+        adventure movies. I have a deep appreciation for nature and a love for international and
+        electronic music.
+      </p>
+      <br />
+      <p>
+        I chose the path of a Software Engineer because it represents a fundamental pillar in the
+        ever-evolving tech world that I hold dear and resonate with. Consequently, I am committed
+        to advancing my hard skills, nurturing my soft skills, and undertaking projects to stay
+        abreast of the dynamic job market.
+      </p>
     </AboutMeSection>
-  )
+  );
 }
