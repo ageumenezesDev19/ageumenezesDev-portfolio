a2decaa94cd9a9ae90b2e5bbcdf6071dcb10ae2b
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-12 11:35:11 -0300

    Update the Experience caulc

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -6,23 +6,38 @@ interface ExperienceProps {
   projectsCount: number;
 }
 
-export function Experience({projectsCount}: ExperienceProps) {
-  return(
+function calculateExperience() {
+  const startDate = new Date(2022, 7, 1);
+  const currentDate = new Date();
+
+  const totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12 
+                    + (currentDate.getMonth() - startDate.getMonth());
+
+  const years = Math.floor(totalMonths / 12);
+  const months = totalMonths % 12;
+
+  const experience = years + months / 12;
+
+  return months === 0 ? years.toString() : experience.toFixed(1);
+}
+
+export function Experience({ projectsCount }: ExperienceProps) {
+  const experience = calculateExperience();
+
+  return (
     <ExperienceSection>
       <div>
         <div>
           <img src={CarreiraImg} alt="Ícone de Carreira" />
           <h4>Experience</h4>
-          <p>+1,5 year</p>
-        {/* Imagem 'carreira'; Font: <a href="https://www.flaticon.com/br/icones-gratis/carreira" title="carreira ícones">Carreira ícones criados por surang - Flaticon</a> */}
+          <p>{experience} years</p>
         </div>
         <div>
           <img src={ProjectManagementImg} alt="Imagem Ícone de Projetos 2" />
           <h4>Projects</h4>
           <p>+{projectsCount} Projects</p>
-          {/* Imagem "project icons"; Font: <a href="https://www.flaticon.com/free-icons/project" title="project icons">Project icons created by dDara - Flaticon</a> */}
         </div>
       </div>
     </ExperienceSection>
-  )
+  );
 }
--- src/components/MyProjects/index.tsx
+++ src/components/MyProjects/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo } from 'react';
 import toDoImg from '../../assets/projects-images/my-tasks-to-do-2.png';
 import solarSystemImg from '../../assets/projects-images/project-solar-system.png';
 import pixelsArtImg from '../../assets/projects-images/porject-3_pixels-art.png';
@@ -10,87 +11,6 @@ import DesignSystem from '../../assets/projects-images/project-design-system.png
 
 import { ProjectsSection, ProjectsSubtitle } from './styles';
 import { ProjectComp } from '../ProjectComp';
-import { useEffect } from 'react';
-
-const projects = [
-  {
-    id: 1,
-    projectName: 'Design System',
-    projectImg: DesignSystem,
-    gitRepository: 'https://github.com/ageumenezesDev19/05-design-system',
-    demoLink: 'https://ageumenezesdev19.github.io/05-design-system/'
-  },
-  {
-    id: 2,
-    projectName: 'FitFlow',
-    projectImg: FitFlowImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/fitflow.git',
-    demoLink: 'https://fitflow-taupe.vercel.app'
-  },
-  {
-    id: 3,
-    projectName: 'Ignite Feed',
-    projectImg: igniteFeedImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/Ignite-feed.git',
-    demoLink: 'https://ignite-feed-livid.vercel.app'
-  },
-  {
-    id: 4,
-    projectName: 'To-Do',
-    projectImg: toDoImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/project-my-tasks-to-do.git',
-    demoLink: 'https://endearing-hamster-35a11e.netlify.app'
-  },
-  {
-    id: 5,
-    projectName: 'Solar System',
-    projectImg: solarSystemImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/project-solar-system',
-    demoLink: 'https://project-solar-system-7s8g9uo88-ageumenezesdev19.vercel.app'
-  },
-  {
-    id: 6,
-    projectName: 'Pixels Art',
-    projectImg: pixelsArtImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/project-trybewarts-2/tree/main',
-    demoLink: 'https://project-pixels-art-alpha.vercel.app'
-  },
-  {
-    id: 7,
-    projectName: 'My Portfolio',
-    projectImg: myPortfolio,
-    gitRepository: 'https://github.com/ageumenezesDev19/ageumenezesDev19P2.github.io-.git',
-  },
-  {
-    id: 8,
-    projectName: 'Trybewarts 2',
-    projectImg: trybeWarts2Img,
-    gitRepository: 'https://github.com/ageumenezesDev19/project-trybewarts-2/tree/main',
-    demoLink: 'https://project-trybewarts-2.vercel.app'
-  },
-  {
-    id: 9,
-    projectName: 'Playground Functions',
-    projectImg: standardImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-04_Intruducão-a-javaScript-e-lógica-de-programacão/dia-5_projeto_playground-functions',
-  },
-  {
-    id: 10,
-    projectName: 'Unit Tests',
-    projectImg: standardImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-4_projeto_javaScript-testes-unitários',
-  },
-  {
-    id: 11,
-    projectName: 'Zoo Functions',
-    projectImg: standardImg,
-    gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/dia-6_projeto_zoo-functions',
-  }
-]
-
-interface DataProps {
-  numberOfProjects: (projects: Project[]) => void;
-}
 
 interface Project {
   id: number;
@@ -100,14 +20,93 @@ interface Project {
   demoLink?: string;
 }
 
-export function MyProjects({numberOfProjects}: DataProps) {
+interface DataProps {
+  numberOfProjects: (projects: Project[]) => void;
+}
+
+export function MyProjects({ numberOfProjects }: DataProps) {
+
+  const projects = useMemo<Project[]>(() => [
+    {
+      id: 1,
+      projectName: 'Design System',
+      projectImg: DesignSystem,
+      gitRepository: 'https://github.com/ageumenezesDev19/05-design-system',
+      demoLink: 'https://ageumenezesdev19.github.io/05-design-system/'
+    },
+    {
+      id: 2,
+      projectName: 'FitFlow',
+      projectImg: FitFlowImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/fitflow.git',
+      demoLink: 'https://fitflow-taupe.vercel.app'
+    },
+    {
+      id: 3,
+      projectName: 'Ignite Feed',
+      projectImg: igniteFeedImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/Ignite-feed.git',
+      demoLink: 'https://ignite-feed-livid.vercel.app'
+    },
+    {
+      id: 4,
+      projectName: 'To-Do',
+      projectImg: toDoImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/project-my-tasks-to-do.git',
+      demoLink: 'https://endearing-hamster-35a11e.netlify.app'
+    },
+    {
+      id: 5,
+      projectName: 'Solar System',
+      projectImg: solarSystemImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/project-solar-system',
+      demoLink: 'https://project-solar-system-7s8g9uo88-ageumenezesdev19.vercel.app'
+    },
+    {
+      id: 6,
+      projectName: 'Pixels Art',
+      projectImg: pixelsArtImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/project-trybewarts-2/tree/main',
+      demoLink: 'https://project-pixels-art-alpha.vercel.app'
+    },
+    {
+      id: 7,
+      projectName: 'My Portfolio',
+      projectImg: myPortfolio,
+      gitRepository: 'https://github.com/ageumenezesDev19/ageumenezesDev19P2.github.io-.git',
+    },
+    {
+      id: 8,
+      projectName: 'Trybewarts 2',
+      projectImg: trybeWarts2Img,
+      gitRepository: 'https://github.com/ageumenezesDev19/project-trybewarts-2/tree/main',
+      demoLink: 'https://project-trybewarts-2.vercel.app'
+    },
+    {
+      id: 9,
+      projectName: 'Playground Functions',
+      projectImg: standardImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-04_Intruducão-a-javaScript-e-lógica-de-programacão/dia-5_projeto_playground-functions',
+    },
+    {
+      id: 10,
+      projectName: 'Unit Tests',
+      projectImg: standardImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-4_projeto_javaScript-testes-unitários',
+    },
+    {
+      id: 11,
+      projectName: 'Zoo Functions',
+      projectImg: standardImg,
+      gitRepository: 'https://github.com/ageumenezesDev19/trybe-course_exercises/tree/main/módulo-1_fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/dia-6_projeto_zoo-functions',
+    }
+  ], []);
 
   useEffect(() => {
-    numberOfProjects(projects)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    numberOfProjects(projects);
+  }, [projects, numberOfProjects]);
 
-  return(
+  return (
     <>
       <ProjectsSubtitle>
         <div>
@@ -115,16 +114,16 @@ export function MyProjects({numberOfProjects}: DataProps) {
         </div>
       </ProjectsSubtitle>
       <ProjectsSection>
-      {projects.map((project) => (
+        {projects.map(({ id, projectName, projectImg, gitRepository, demoLink }) => (
           <ProjectComp
-            key={project.id}
-            projectName={project.projectName}
-            projectImg={project.projectImg}
-            gitRepository={project.gitRepository}
-            demoLink={project.demoLink}
+            key={id}
+            projectName={projectName}
+            projectImg={projectImg}
+            gitRepository={gitRepository}
+            demoLink={demoLink}
           />
         ))}
       </ProjectsSection>
     </>
-  )
+  );
 }
